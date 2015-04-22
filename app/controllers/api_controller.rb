b61d949dbfe745c1e385eb31071430bf5926afff
Arjun Venkataswamy <avenkat2@gmail.com>
2015-04-22 13:32:52 -0500

    adds solutions

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -8,9 +8,29 @@ class ApiController < ApplicationController
   def weather_output
     address = URI.encode(params["address"])
     url = "http://api.openweathermap.org/data/2.5/weather?q=#{address}"
-    initial_info = open(url).read
-    parsed_info = JSON.parse(initial_info)
-    @result = parsed_info["weather"][0]["main"]
-    @icon = parsed_info["weather"][0]["icon"]
+    response = open(url).read
+    parsed_response = JSON.parse(response)
+    @result = parsed_response["weather"][0]["description"]
+    @icon = parsed_response["weather"][0]['icon']
+    @lat = parsed_response["coord"]["lat"]
+    @lon = parsed_response["coord"]["lon"]
+    @wind_speed = parsed_response['wind']['speed']
+    deg = parsed_response['wind']['deg']
+    if deg < 90
+      @wind_direction = "NE"
+    elsif deg < 180
+      @wind_direction = "SE"
+    elsif deg < 270
+      @wind_direction = "SW"
+    elsif deg < 360
+      @wind_direction = "NW"
+    end
+    min_kelvin = parsed_response['main']['temp_min']
+    @min_temp = (min_kelvin - 273.15)* 1.8000 + 32.00
+
+
+    max_kelvin = parsed_response['main']['temp_max']
+    @max_temp = (max_kelvin - 273.15)* 1.8000 + 32.00
+
   end
 end
